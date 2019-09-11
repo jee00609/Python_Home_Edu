9f965fd50307a999d7f91155d53cd4f0e73a1371
jee00609 <31675804+jee00609@users.noreply.github.com>
2019-09-11 23:24:11 +0900

    키보드 입력
    C의 scanf() 와 같은 역할

---
+++ 일단 만들어보는 계산기와 그림판 프로그램/02_02.py
@@ -0,0 +1,14 @@
+a=input()
+b=input()
+
+result=a+b
+print(a,"+",b,"=",result)
+
+result=a-b
+print(a,"-",b,"=",result)
+
+result=a*b
+print(a,"*",b,"=",result)
+
+result=a/b
+print(a,"/",b,"=",result)
